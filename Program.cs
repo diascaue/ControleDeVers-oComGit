bb64d6dfa338c6021b7eb4208ac25d540a91f03e
Cauê Dias <diasco0612@gmail.com>
2024-04-06 13:51:47 -0300

    Adicionada linha com versão da aplicação.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine(DateTime.Now.ToLongDateString());
+Console.WriteLine("versão da aplicação 1.0");
